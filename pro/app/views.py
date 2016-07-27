981e6dd6600fb40a6f1654d843ab9fc776f0bbd8
Vinit Shah <vinit.shah@wishabi.com>
2016-07-27 17:34:39 -0400

    first push

---
+++ pro/app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("Flipp Performance Tracking Optimizer 3000")
+# Create your views here.
