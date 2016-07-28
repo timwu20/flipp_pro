d9c85b5b574b9c298bf336922bc76201f04625e3
Timothy Wu <timothy.wu@flipp.com>
2016-07-28 11:51:02 -0400

    removed old app

--- pro/app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("Flipp Performance Tracking Optimizer 3000")
-# Create your views here.
