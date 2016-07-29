311c24753671b8286d1e6bd05d87f4f169ad2fb1
Timothy Wu <timothy.wu@flipp.com>
2016-07-29 10:58:33 -0400

    more base models

--- pro/pro/app/admin.py
+++ pro/pro/app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from pro.app.models import Employee, Approver, Coach, SelfEvaluation, PerformanceReview, Feedback360, CaseStatus, Case
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Approver)
+admin.site.register(Coach)
+admin.site.register(SelfEvaluation)
+admin.site.register(PerformanceReview)
+admin.site.register(Feedback360)
+admin.site.register(CaseStatus)
+admin.site.register(Case)
--- pro/pro/app/models.py
+++ pro/pro/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import signals
 
 class Employee(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,16 +10,73 @@ class Employee(models.Model):
 	department = models.CharField(max_length=100)
 	phone_number = models.CharField(max_length=10)
 
-class Coach(Employee):
+	def __str__(self):
+		return '%s' % self.user
 
-	employee_ptr = models.OneToOneField('Employee', related_name="%(app_label)s_%(class)s_related")
+def create_employee(sender, instance, created, **kwargs):
+    Employee.objects.get_or_create(user=instance)
+
+signals.post_save.connect(create_employee, sender=User, weak=False, dispatch_uid='create_employee')
+
+class Approver(Employee):
+	employee = models.OneToOneField(Employee, related_name="%(app_label)s_%(class)s_related")
+
+	def __str__(self):
+		return self.employee
+
+class ApproverCoach(models.Model):
+	approver = models.ForeignKey(Approver, on_delete=models.CASCADE)
+	coach = models.ForeignKey('Coach', on_delete=models.CASCADE)
+	sequence_order = models.IntegerField(default=0)
+
+	def next_sequence_order(self):
+		#not incrementing cause we can use 0 index
+		return ApproverCoach.objects.filter(approver=self.approver, coach=self.coach).count()
+
+
+class Coach(Employee):
+	approvers = models.ManyToManyField(Approver, through=ApproverCoach, related_name='coaches')
+	employee = models.OneToOneField(Employee, related_name="%(app_label)s_%(class)s_related")
 
 	def get_num_employees():
 		return self.employees.count()
 
+	def __str__(self):
+		return self.employee.user
 
-		
-		
+	class Meta:
+		verbose_name_plural = 'coaches'
 
+class BaseForm(models.Model):
+	case = models.ForeignKey('Case')
+	submitted = models.BooleanField(default=False)
+	submitted_date = models.DateTimeField(null=True, default=None)
 
+	class Meta:
+		abstract = True
 
+class SelfEvaluation(BaseForm):
+	pass
+
+class PerformanceReview(BaseForm):
+	pass
+
+class Feedback360(BaseForm):
+	provider = models.ForeignKey(Employee)
+
+class CaseStatus(models.Model):
+	status = models.CharField(max_length=100, blank=False, null=False)
+
+class Case(models.Model):
+	#people
+	employee = models.ForeignKey(Employee)
+	coach = models.ForeignKey(Coach, related_name='cases')
+
+	#forms
+	self_evaluation_form = models.OneToOneField(SelfEvaluation, related_name='cases')
+	performance_review = models.OneToOneField(PerformanceReview, related_name='cases')
+
+	feedback_360_forms = models.ForeignKey(Feedback360, related_name='cases')
+
+	#status
+	status = models.ForeignKey(CaseStatus)
