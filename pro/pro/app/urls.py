48dece2b9e6eeafb958932335b4ac52fe94ce0f2
Leon Lin <ylin9720@gmail.com>
2016-08-01 23:15:57 -0400

    found my viewssss

--- pro/pro/app/urls.py
+++ pro/pro/app/urls.py
@@ -2,8 +2,12 @@ from django.conf.urls import url
 from . import views
 import django_cas_ng
 
+app_name = 'app'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+	url(r'^(?P<coach_id>[0-9]+)/add/$', views.addTeam, name='add'),
+
+	## SSO ##
     url(r'^accounts/login$', django_cas_ng.views.login, name='cas_ng_login'),
     url(r'^accounts/logout$', django_cas_ng.views.logout, name='cas_ng_logout'),
     url(r'^accounts/callback$', django_cas_ng.views.callback, name='cas_ng_proxy_callback'),
--- pro/pro/app/views.py
+++ pro/pro/app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django_cas_ng import views as baseviews
 from django.views.decorators.csrf import csrf_exempt
-from pro.app.models import Case
+from pro.app.models import Case, Employee, Coach
 
 def index(request):
     if request.user.employee.coach:
@@ -11,3 +11,28 @@ def index(request):
         'employee': request.user.employee,
         'open_cases': cases, 
     })
+
+def addTeam(request, coach_id):
+	coach = Coach.objects.get(pk=coach_id)
+	added = false
+	if not request.method == 'POST':
+		employee_list = Employees.objects.all()
+		employee_list = filter((lambda x: not x in coach.employees), employee_list)
+		return render(request, 'team.html', {
+			'employee_list': employee_list,
+			'added': added
+		})
+	new_employees = request.POST.getlist('employee')
+	for employee in new_employees: 
+		print(employee)
+		employee.coach = coach
+		employee.save()
+	coach.save()
+	added = true
+	return render(request, 'team.html', {
+		'added': added
+		})
+		
+
+
+	
